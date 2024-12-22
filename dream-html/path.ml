b74c5a24bd7cc16c4f1270526771bb2ee2095149
Yawar Amin <yawar.amin@gmail.com>
2024-12-22 13:11:47 -0500

    Add copyright notices

--- dream-html/path.ml
+++ dream-html/path.ml
@@ -1,3 +1,20 @@
+(* Copyright 2024 Yawar Amin
+
+   This file is part of dream-html.
+
+   dream-html is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option) any
+   later version.
+
+   dream-html is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along with
+   dream-html. If not, see <https://www.gnu.org/licenses/>. *)
+
 type ('r, 'p) t =
   { rfmt : ('r, unit, Dream.response Dream.promise) format;
     afmt : ('p, unit, string, Pure_html.attr) format4
